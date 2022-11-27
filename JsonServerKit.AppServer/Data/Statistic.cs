03cf3fc23aafd826cbc2241b786d3e548bf8ca56
jsonserverkit <jsonserverkit@gmail.com>
2022-11-28 00:55:24 +0100

    Performance statistic data dump to database. Beta. Refactoring and placement tbd.

---
+++ JsonServerKit.AppServer/Data/Statistic.cs
@@ -0,0 +1,12 @@
+﻿namespace JsonServerKit.AppServer.Data
+{
+    public class PayloadStatistic
+    {
+        public Payload Payload { get; set; }
+
+        public double TimeInMsMessageSent { get; set; }
+        public double TimeInMsMessageReceived { get; set; }
+        public string MessageType { get; set; }
+    }
+
+}
